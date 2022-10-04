6eff202c89f7b13c6b37b9a66a5aab0c48c476ae
Julia <pushistik12131@mail.ru>
2022-10-04 20:58:31 +0300

    add task

---
+++ Task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] arr1 = new string[4] {"hello", "2", "world", ":-)"};
+string[] arr2 = new string[arr1.Length];
+
+void new_array(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if (arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write("{0} ", array[i]);
+        }
+        Console.Write("]");
+        Console.WriteLine();
+}
+new_array(arr1, arr2);
+PrintArray(arr2);
